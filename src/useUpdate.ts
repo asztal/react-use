033e322b97b27546a8129eaf17757f5ab9e0162f
Lee Houghton <asztal@users.noreply.github.com>
2020-03-10 11:16:27 +0000

    Use useReducer for useState
    I know this is micro-optimisation, so feel free to ignore it, but `useCallback` wasn't necessary here.

--- src/useUpdate.ts
+++ src/useUpdate.ts
@@ -1,11 +1,12 @@
-import { useCallback, useState } from 'react';
+import { useReducer } from "react";
 
 const incrementParameter = (num: number): number => ++num % 1_000_000;
 
 const useUpdate = () => {
-  const [, setState] = useState(0);
-  // useCallback with empty deps as we only want to define updateCb once
-  return useCallback(() => setState(incrementParameter), []);
+  // Use useReducer as the `dispatch` function is guaranteed not to change
+  // https://reactjs.org/docs/hooks-reference.html#usereducer
+  const [, dispatch] = useReducer(incrementParameter, 0);
+  return dispatch;
 };
 
 export default useUpdate;
